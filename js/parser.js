800bdcc5f4d0a144b74b725c4d2f2fdd94463cd3
Albertov91 <albertov91@gmail.com>
2017-03-14 14:12:21 +0100

    Beautified functional version (v1.0)
    - Functions completed (search, labels).
    - Some bugs fixed (history, platform parsing).
    - Beautified (based on the original EDB site).

--- js/parser.js
+++ js/parser.js
@@ -1,8 +1,13 @@
-
+/*!
+ * AJAX EDB v1.0 (https://albertov91.github.io/ajax-edb/)
+ * Licensed under MIT (https://github.com/albertov91/ajax-edb/blob/master/LICENSE)
+ */
+ 
 function parse(rawData){
 
 	arrayData = $.csv.toArrays(rawData);
 	arrayData.shift(); // Removes the header.
+	
 	// localStorage.setItem("arrayData",JSON.stringify(arrayData));
 	loadParsedData();
 	
@@ -11,6 +16,9 @@ function parse(rawData){
 
 function loadParsedData(){
 	var categories = ["remote","webapps","local","dos","shellcode"];
+	
+	$('#results').html("");
+	document.title = "AJAX EDB";	
 	fillTopTables(categories);
 }
 
@@ -29,8 +37,7 @@ function filterByCategory(category){
 	var result = [];
 	
 	for(var row in arrayData) {
-		var filePath = arrayData[row][1];
-		var row_category = filePath.split("/")[2];
+		var row_category = arrayData[row][6];
 		if(row_category === category){
 			result.push(arrayData[row]);
 		}
@@ -43,37 +50,63 @@ function filterByCategory(category){
 // build HTML table data from an array (one or two dimensional)
 function generateTable(data) {
   var html = '';
-
+  var sorted_row = [];
+  
   if(typeof(data[0]) === 'undefined') {
 	return null;
   }
 
-  if(data[0].constructor === String) {
-	html += '<tr>\r\n';
-	for(var item in data) {
-	  html += '<td>' + data[item] + '</td>\r\n';
-	}
-	html += '</tr>\r\n';
-  }
-
   if(data[0].constructor === Array) {
+	html += '<table class="exploit_list bootstrap-wrapper">';
+	// Table headers
+	html += '<thead>\r\n';
+	html += '<tr>\r\n';
+	html += '<th class="text-center date">Date Added</td>\r\n';
+	html += '<th class="text-center">D</td>\r\n';
+	html += '<th class="description">Title</td>\r\n';
+	html += '<th class="text-center">Platform</td>\r\n';
+	html += '<th class="text-center">Author</td>\r\n';
+	html += '</tr>\r\n';
+	html += '</thead>\r\n';
+	html += '<tbody>\r\n';
 	for(var row in data) {
 	  html += '<tr>\r\n';
-	  for(var item in data[row]) {
-		html += '<td>' + data[row][item] + '</td>\r\n';
+	  sorted_row[0] = data[row][3]; //Date
+	  sorted_row[1] = data[row][0]; //ID
+	  sorted_row[2] = data[row][2]; //Title
+	  sorted_row[3] = data[row][5]; //Platform
+	  sorted_row[4] = data[row][4]; //Author
+	  for(var item in sorted_row) {
+		switch(parseInt(item)){
+			case 0:
+				html += '<td class="date">';
+				html += sorted_row[item] + '</td>\r\n';
+				break;
+			case 1:
+				html += '<td class="dlink"><a href="https://www.exploit-db.com/download/' + sorted_row[1] + '/">';
+				html += '<img src="img/download_icon.png" width="16px" height="16px" style="vertical-align:middle" /></a></td>\r\n'
+				break;				
+			case 2:
+				html += '<td class="description"><a href="https://www.exploit-db.com/exploits/' + sorted_row[1] + '/" target="_blank">';
+				html += sorted_row[item] + '</a></td>\r\n';
+				break;		
+			case 3:
+				html += '<td class="platform">';
+				html += sorted_row[item] + '</td>\r\n';
+				break;
+			case 4:
+				html += '<td class="author">';
+				html += sorted_row[item] + '</td>\r\n';
+				break;	
+			default:
+				console.log("Item not registered: "+item);
+		}
 	  }
+	  sorted_row = [];
 	  html += '</tr>\r\n';
 	}
-  }
-
-  if(data[0].constructor === Object) {
-	for(var row in data) {
-	  html += '<tr>\r\n';
-	  for(var item in data[row]) {
-		html += '<td>' + item + ':' + data[row][item] + '</td>\r\n';
-	  }
-	  html += '</tr>\r\n';
-	}
+	html += '</tbody>\r\n';
+	html += '</table>\r\n';
   }
   
   return html;
--- js/search.js
+++ js/search.js
@@ -1,4 +1,8 @@
-
+/*!
+ * AJAX EDB v1.0 (https://albertov91.github.io/ajax-edb/)
+ * Licensed under MIT (https://github.com/albertov91/ajax-edb/blob/master/LICENSE)
+ */
+ 
 function bind_search(){
 	$("#searchTxt").keydown(function(event){
 		if (event.which === 13) {
@@ -26,13 +30,44 @@ function search(pre_query){
 	results = arrayData.slice();
 	// Search using AND operator for each keyword found.
 	for(i in keywords) {
-		results = $.grep(results, function(row) {
-			return (row[2].toLowerCase().indexOf(keywords[i].toLowerCase()) >= 0);
-		});
+		var keyword = keywords[i].toLowerCase();
+		
+		if(keyword.indexOf("platform:") == 0){
+			//Label 'platform:'
+			var platform = keyword.split(":")[1];
+			results = $.grep(results, function(row) {
+				return (row[5].toLowerCase().indexOf(platform) >= 0);
+			});
+		}else if(keyword.indexOf("author:") == 0){
+			//Label 'author:'
+			var author = keyword.split(":")[1];
+			results = $.grep(results, function(row) {
+				return (row[4].toLowerCase().indexOf(author) >= 0);
+			});	
+		}else if(keyword.indexOf("filetype:") == 0){
+			//Label 'filetype:'
+			var filetype = keyword.split(":")[1];
+			results = $.grep(results, function(row) {
+				return (row[1].toLowerCase().endsWith("."+filetype));
+			});			
+		}else{
+			var invert = false;
+			if(keyword.indexOf("-") == 0){
+				invert = true; //Exclude keyword.
+				keyword = keyword.substring(1);
+			}
+			//Keywords
+			results = $.grep(results, function(row) {
+				return (row[2].toLowerCase().indexOf(keyword) >= 0);
+			}, invert);
+		}
 	}
 	
 	var htmlData = generateTable(results.reverse());
-	$('#panel').html(htmlData);
+	// Fills the web content.
+	htmlData = '<h2 style="text-align: justify;"><a href="#"> Search Results for "' + $("<span>").text(search_query).html() + '" </a></h2>' + '<p style="text-align:justify;">' + results.length + ' results found</p>'  + htmlData;
+	$('#results').html(htmlData);
+	$('#status').html("");
 	if (pre_query == null){
 		history.pushState(search_query, null);	
 	} 
